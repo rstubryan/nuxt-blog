8a33bfd835b56199a1bdb8c7cf72419dff688ab8
rstubryan <restubumirr@gmail.com>
2025-09-10 19:28:40 +0700

    feat: update Nuxt configuration with path aliases and add @types/node dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -8,18 +9,29 @@ export default defineNuxtConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@components": fileURLToPath(
+          new URL("./app/components", import.meta.url),
+        ),
+        "@composables": fileURLToPath(
+          new URL("./app/composables", import.meta.url),
+        ),
+        "@assets": fileURLToPath(new URL("./app/assets", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./app/layouts", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./app/pages", import.meta.url)),
+        "@middleware": fileURLToPath(
+          new URL("./app/middleware", import.meta.url),
+        ),
+        "@utils": fileURLToPath(new URL("./app/utils", import.meta.url)),
+        "@lib": fileURLToPath(new URL("./app/lib", import.meta.url)),
+      },
+    },
   },
 
   modules: ["shadcn-nuxt"],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
     componentDir: "./app/components/ui",
   },
 });
