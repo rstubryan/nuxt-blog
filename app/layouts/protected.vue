6ccbb3c5ef0702487ed843b9d592ae4bdff8e8c0
rstubryan <restubumirr@gmail.com>
2025-09-13 21:14:47 +0700

    feat: add new sidebar and sheet components with collapsible functionality

--- app/layouts/protected.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex h-screen w-full items-center justify-center px-4">
-    <slot />
-  </div>
-</template>
--- app/pages/(protected)/dashboard/index.vue
+++ app/pages/(protected)/dashboard/index.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import { useHead } from "nuxt/app";
-import { useLogoutMutation } from "@/composables/services/auth/mutation";
 import { ref, onMounted } from "vue";
 import { Loader2 } from "lucide-vue-next";
+
+import AppSidebar from "@/components/AppSidebar.vue";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 useHead({
   title: "Dashboard",
 });
 
-const { logout } = useLogoutMutation();
 const isVerifying = ref(true);
 
 onMounted(() => {
@@ -19,20 +33,50 @@ onMounted(() => {
 </script>
 
 <template>
-  <NuxtLayout name="protected">
-    <template v-if="isVerifying">
-      <div class="flex justify-center items-center h-full">
-        <Loader2 class="animate-spin h-8 w-8 text-gray-500" />
-      </div>
-    </template>
-    <template v-else>
-      Hello World
-      <button
-        class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        @click="logout"
-      >
-        Logout
-      </button>
-    </template>
-  </NuxtLayout>
+  <template v-if="isVerifying">
+    <div class="flex h-screen w-full items-center justify-center">
+      <Loader2 class="animate-spin h-8 w-8 text-gray-500" />
+    </div>
+  </template>
+  <template v-else>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header
+          class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+        >
+          <div class="flex items-center gap-2 px-4">
+            <SidebarTrigger class="-ml-1" />
+            <Separator
+              orientation="vertical"
+              class="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem class="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator class="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div class="bg-muted/50 aspect-video rounded-xl" />
+            <div class="bg-muted/50 aspect-video rounded-xl" />
+            <div class="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div
+            class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"
+          />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  </template>
 </template>
